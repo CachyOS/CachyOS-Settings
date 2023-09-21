6e8ac402bfaad0954ca6c0abb6539a9fb3afbd01
Norz3n <norz3nmusic@gmail.com>
2023-09-21 14:48:54 +0300

    Added wayland variables to profile.d

---
+++ etc/profile.d/wayland.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    export WAYLAND=1
+    export QT_QPA_PLATFORM='xcb'
+    export GDK_BACKEND='wayland,x11'
+    export MOZ_DBUS_REMOTE=1
+    export MOZ_ENABLE_WAYLAND=1
+    export _JAVA_AWT_WM_NONREPARENTING=1
+    export BEMENU_BACKEND=wayland
+    export CLUTTER_BACKEND=wayland
+    export ECORE_EVAS_ENGINE=wayland_egl
+    export ELM_ENGINE=wayland_egl
+    export WINIT_UNIX_BACKEND=x11
+    export KITTY_ENABLE_WAYLAND=1
+fi
